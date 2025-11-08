ac5f09883d6036df8661b6f361497b26242fa448
d0ugal <48211+d0ugal@users.noreply.github.com>
2025-11-08 21:23:43 +0000

    chore: apply code formatting changes

--- linter/prometheuslabels/analyzer.go
+++ linter/prometheuslabels/analyzer.go
@@ -44,4 +44,3 @@ func run(pass *analysis.Pass) (interface{}, error) {
 
 	return nil, nil
 }
-
--- linter/prometheuslabels/cmd/prometheuslabels/main.go
+++ linter/prometheuslabels/cmd/prometheuslabels/main.go
@@ -2,11 +2,9 @@ package main
 
 import (
 	"github.com/d0ugal/promexporter/linter/prometheuslabels"
-
 	"golang.org/x/tools/go/analysis/singlechecker"
 )
 
 func main() {
 	singlechecker.Main(prometheuslabels.Analyzer)
 }
-
